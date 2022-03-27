3c151182f42f5c3e7db13d5d48e6e15da8423fc9
Ryan Anderson <sonorousduck@gmail.com>
2022-03-27 12:24:35 -0600

    Added EnemyTag and Enemy Abstract class

---
+++ TowerDefense/Enemy/EnemyTag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CrowEngineBase;
+
+namespace TowerDefense
+{
+    public class EnemyTag : Component
+    {
+        public EnemyType enemyType { get; set; }
+
+        public EnemyTag(EnemyType enemyType)
+        {
+            this.enemyType = enemyType;
+        }
+    }
+
+
+    public enum EnemyType
+    {
+        AIR,
+        GROUND,
+        MIXED,
+    }
+
+}
