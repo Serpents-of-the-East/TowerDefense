932f234c352ff3f37248dcba2475869df37387be
Ryan Anderson <sonorousduck@gmail.com>
2022-03-29 16:41:56 -0600

    Added health component

---
+++ Health/PlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CrowEngineBase;
+
+namespace TowerDefense
+{
+    public class PlayerHealth
+    {
+        public int healthPoints { get; set; }
+
+        public PlayerHealth()
+        {
+            healthPoints = 10;
+        }
+    }
+}
