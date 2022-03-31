01245f3d60c00279323bb3131ca76b5454051845
Ryan Anderson <sonorousduck@gmail.com>
2022-03-30 19:41:58 -0600

    Added functionality to CrowMath

--- CrowEngineBase/Utilities/CrowMath.cs
+++ CrowEngineBase/Utilities/CrowMath.cs
@@ -11,5 +11,25 @@ namespace CrowEngineBase.Utilities
             return start + (end - start) * step;
         }
 
+        public static bool Tolerance(float current, float target, float tolerance)
+        {
+            return MathF.Abs(current - target) < tolerance;
+        }
+
+        public static bool Tolerance(Vector2 current, Vector2 target, float tolerance)
+        {
+            return Vector2.DistanceSquared(current, target) < tolerance * tolerance;
+        }
+
+        public static float Angle(Vector2 current, Vector2 target)
+        {
+            return MathF.Acos(Vector2.Dot(Vector2.Normalize(current), Vector2.Normalize(target)));
+        }
+
+        public static float AngleDegrees(Vector2 current, Vector2 target)
+        {
+            return (180 / MathF.PI) * MathF.Acos(Vector2.Dot(Vector2.Normalize(current), Vector2.Normalize(target)));
+        }
+
     }
 }
