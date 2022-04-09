57db15fb0b0995e7170e806f580b4c0247309a07
Ryan Anderson <sonorousduck@gmail.com>
2022-04-09 08:52:38 -0600

    ParticleSystem lifetime working

--- Prefabs/ParticlePrefabs/SellParticles.cs
+++ Prefabs/ParticlePrefabs/SellParticles.cs
@@ -13,15 +13,15 @@ namespace TowerDefense
             Particle particle = new Particle(ResourceManager.GetTexture("coin"));
 
             particle.rate = TimeSpan.FromMilliseconds(10);
-            particle.maxSpeed = 1000;
+            particle.maxSpeed = 200;
             particle.minSpeed = 100;
-            particle.minScale = 0.5f;
-            particle.maxScale = 1;
-            particle.rotationSpeed = 5f;
+            particle.minScale = .5f;
+            particle.maxScale = 1f;
+            particle.rotationSpeed = 1f;
             particle.renderDepth = 1;
             particle.maxLifeTime = TimeSpan.FromMilliseconds(100);
-            particle.emissionArea = Vector2.One * 0.5f;
-
+            particle.emissionArea = Vector2.One * 25f;
+            particle.maxSystemLifetime = TimeSpan.FromMilliseconds(500);
 
             gameObject.Add(particle);
             gameObject.Add(new Transform(position, 0, Vector2.One));
